dae436a147662559a2df828a9c0e7aedae9b77ca
yamosan <yamorimasato@gmail.com>
2022-01-16 04:46:51 +0900

    疎通確認

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,10 +1,57 @@
 import type { NextPage } from "next";
 
+import { useCallback, useEffect, useState } from "react";
+import type { FormEvent } from "react";
+import { io, Socket } from "socket.io-client";
+
 const Home: NextPage = () => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+  useEffect(() => {
+    const socketIo = io("http://localhost:5000");
+    setSocket(socketIo);
+
+    socketIo.on("connect", () => {
+      console.log("socket connected");
+    });
+
+    socketIo.on("pong", (data) => {
+      console.log("[pong]", data);
+    });
+
+    return () => {
+      if (socket) socket.disconnect();
+    };
+  }, []);
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const target = e.currentTarget as typeof e.currentTarget & {
+        message: { value: string };
+      };
+
+      socket.emit("ping", target.message.value);
+    },
+    [socket]
+  );
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Message</h1>
+      <form onSubmit={onSubmit} className="flex items-center space-x-2">
+        <input
+          type="text"
+          name="message"
+          className="px-2 border-2 border-gray-400"
+        />
+        <button
+          type="submit"
+          className="px-3 py-1 bg-blue-500 text-white rounded w-min"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,10 +6,28 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const http = createServer(app);
-const io = new Server(http);
+const io = new Server(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+io.on("connection", (socket) => {
+  console.log(`[${socket.id}] connected`);
+
+  socket.on("ping", (data) => {
+    console.log(`[ping]: ${data} @${socket.id}`);
+    socket.emit("pong", "pong");
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`[${socket.id}] disconnected`);
+  });
 });
 
 http.listen(PORT);
