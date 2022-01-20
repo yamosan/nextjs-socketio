4b7fc32bf7d37adf2647c22f4ec043eba08849df
yamosan <yamorimasato@gmail.com>
2022-01-21 05:06:42 +0900

    背景色を変更

--- packages/client/src/shared/components/layout/Layout.tsx
+++ packages/client/src/shared/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const Layout: VFC<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gray-100 overflow-x-hidden">
+    <div className="min-h-screen bg-blue-50 overflow-x-hidden">
       <header className="fixed h-16 w-full flex justify-center items-center bg-white border-b">
         <div className="flex justify-start w-full max-w-[1000px] mx-auto">
           <h1 className="text-2xl font-bold text-gray-800">Chat</h1>
