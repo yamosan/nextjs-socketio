504f311be11e259b1e680f64af0256408d833cf8
yamosan <yamorimasato@gmail.com>
2022-01-21 06:50:32 +0900

    最大幅のユーティリティを追加

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,8 +1,12 @@
+const APP_MAX_WIDTH = "1100px";
+
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: { app: APP_MAX_WIDTH },
+    },
   },
   variants: { opacity: ["disabled"] },
   plugins: [],
