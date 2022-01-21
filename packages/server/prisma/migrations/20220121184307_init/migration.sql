fa39f0912ebc509d58e876632a5f6e7ccf5a5bc3
yamosan <yamorimasato@gmail.com>
2022-01-22 04:11:51 +0900

    prismaを追加

---
+++ packages/server/prisma/migrations/20220121184307_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "senderId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,17 +7,13 @@ import type {
   InterServerEvents,
   SocketData,
 } from "@project/common/types/socketIO";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const http = createServer(app);
-const io = new Server<
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData
->(http, {
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(http, {
   // TODO
   cors: {
     origin: "*",
@@ -25,8 +21,23 @@ const io = new Server<
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/messages", async (req, res) => {
+  const messages = await prisma.message.findMany({
+    orderBy: { createdAt: "asc" },
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      sender: {
+        select: {
+          id: true,
+          name: true,
+          avatarUrl: true,
+        },
+      },
+    },
+  });
+  res.json(messages);
 });
 
 io.on("connection", (socket) => {
