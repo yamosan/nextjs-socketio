26f6481b4ca6a2b1482369b15712e64de5589714
yamosan <yamorimasato@gmail.com>
2022-01-21 20:32:03 +0900

    Topページのデザインを修正

--- packages/client/src/modules/Top/Top.tsx
+++ packages/client/src/modules/Top/Top.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import type { FormEvent } from "react";
 import { useStore } from "../../stores/store";
 import { useRouter } from "next/router";
@@ -31,33 +31,33 @@ export const Top: NextPage = () => {
   if (loading) return null;
   return (
     <div className="flex flex-col justify-center items-center h-full">
-      <h1 className="text-2xl font-bold mb-4">Top Page</h1>
+      <h1 className="text-2xl font-bold mb-6">Set up your profile</h1>
 
       <div className="relative mb-4">
         {emoji && <Avatar src={emoji.src} size={96} alt={emoji.name} />}
-        <button type="button" onClick={update} className="absolute -right-2 -bottom-2">
+        <button type="button" onClick={update} className="absolute -right-0.5 -bottom-0.5 text-indigo-400">
           <RefreshIcon className="w-6 h-6" />
         </button>
       </div>
 
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col items-start">
-          <label className="text-xs block text-gray-600">名前</label>
+          <label className="text-xs block text-gray-400">name</label>
           <input
             type="text"
             name="name"
             maxLength={20}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="px-2 border-2 border-gray-400 rounded"
+            className="px-2 rounded shadow-sm shadow-gray-200 h-7"
           />
         </div>
         <button
           type="submit"
           disabled={!username}
-          className="mt-4 mx-auto block px-3 py-1 bg-blue-500 text-white rounded disabled:opacity-50"
+          className="mt-4 mx-auto block px-6 py-1.5 bg-indigo-400 text-white rounded disabled:opacity-50"
         >
-          参加する
+          join
         </button>
       </form>
     </div>
