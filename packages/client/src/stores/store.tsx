e354b3d36bda987f4b0fcf5074b0ffcdfc1b7309
yamosan <yamorimasato@gmail.com>
2022-01-21 20:00:47 +0900

    reducerを使用

--- packages/client/src/stores/store.tsx
+++ packages/client/src/stores/store.tsx
@@ -1,25 +1,50 @@
-import type { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useReducer } from "react";
 import { createContext, useContext, useState } from "react";
 
-type TStoreContext = {
-  username: string;
-  setUsername: Dispatch<SetStateAction<string>>;
+type Avatar = {
+  src: string;
+  alt: string;
 };
 
-const StoreContext = createContext<TStoreContext | null>(null);
+type User = {
+  name: string;
+  avatar: Avatar;
+};
+
+type TStore = {
+  user: User | null;
+};
+
+const initialStore: TStore = {
+  user: null,
+};
+
+type TAction = { type: "signin"; payload: { user: User } };
+
+const reducer: React.Reducer<TStore, TAction> = (state, action) => {
+  switch (action.type) {
+    case "signin":
+      return { ...action.payload };
+    default:
+      return state;
+  }
+};
+
+type TStoreContext = {
+  state: TStore;
+  dispatch: Dispatch<TAction>;
+};
+
+const StoreContext = createContext<TStoreContext>({} as TStoreContext);
 
 // Hooks
 export const useStore = () => useContext(StoreContext);
 
 // Provider
 const StoreProvider: React.FC = ({ children }) => {
-  const [username, setUsername] = useState<string>("");
+  const [state, dispatch] = useReducer(reducer, initialStore);
 
-  return (
-    <StoreContext.Provider value={{ username, setUsername }}>
-      {children}
-    </StoreContext.Provider>
-  );
+  return <StoreContext.Provider value={{ state, dispatch }}>{children}</StoreContext.Provider>;
 };
 
 export default StoreProvider;
