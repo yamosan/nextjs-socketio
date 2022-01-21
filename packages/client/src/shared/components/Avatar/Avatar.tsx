f1af28f2290fbe5d7abb94ea39f16fa94bebc061
yamosan <yamorimasato@gmail.com>
2022-01-21 20:01:22 +0900

    余白を設定できるようにする

--- packages/client/src/shared/components/Avatar/Avatar.tsx
+++ packages/client/src/shared/components/Avatar/Avatar.tsx
@@ -1,19 +1,20 @@
-import { memo, VFC } from "react";
+import type { ComponentProps, VFC } from "react";
+import { memo } from "react";
 import Image from "next/image";
 
 type Props = {
-  id: string;
-};
+  size: number;
+  padding?: number;
+} & Omit<ComponentProps<typeof Image>, "width" | "height">;
 
-const _Avatar: VFC<Props> = ({ id }) => {
+const _Avatar: VFC<Props> = ({ size, padding, ...attrs }) => {
+  const pad = padding || size / 4;
   return (
-    <div className="aspect-square flex justify-center items-center rounded-full overflow-hidden">
-      <Image
-        src={`https://i.pravatar.cc/48/?u=${id}`}
-        width={48}
-        height={48}
-        alt="avatar"
-      />
+    <div
+      className="aspect-square flex justify-center items-center rounded-full overflow-hidden bg-slate-200"
+      style={{ padding: `${pad}px` }}
+    >
+      <Image width={size - pad * 2} height={size - pad * 2} {...attrs} />
     </div>
   );
 };
