265aafedd39c86cb183ece14e169f98837a94eae
yamosan <yamorimasato@gmail.com>
2022-01-21 20:21:15 +0900

    ヘッダーロゴをリンクにする

--- packages/client/src/shared/components/layout/Layout.tsx
+++ packages/client/src/shared/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { ReactNode, VFC } from "react";
 import { useStore } from "../../../stores/store";
 import { Avatar } from "../Avatar";
@@ -12,7 +13,11 @@ export const Layout: VFC<Props> = ({ children }) => {
     <div className="min-h-screen h-0 bg-slate-100 overflow-x-hidden">
       <header className="fixed h-16 w-full flex justify-center items-center bg-white border-b">
         <div className="flex justify-between items-center w-full max-w-app mx-auto">
-          <h1 className="text-2xl font-bold text-gray-800">Chat</h1>
+          <Link href="/">
+            <a>
+              <h1 className="text-2xl font-bold text-gray-800">Chat</h1>
+            </a>
+          </Link>
           <div>{state.user && <Avatar src={state.user.avatarUrl} size={32} alt={state.user.name} />}</div>
         </div>
       </header>
